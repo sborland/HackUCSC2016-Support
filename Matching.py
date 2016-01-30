9ae5a99947759ff43ceefeeb8c5068b1917c46bc
Henry Pan <henry-pan@users.noreply.github.com>
2016-01-29 20:12:29 -0800

    Some changes

--- Matching.py
+++ Matching.py
@@ -1,63 +1,54 @@
 # ==================================
 # Matching.py
 # 
-# Matching.py [Victim's Attributes] [[Volunteer's Atts],...]
+# Matching.py [client's Attributes] [[Volunteer's Atts],...]
 # 
 # Lists must have at least three attributes 
 #
 # This algorithm will match volunteers
-# to victims depending on their
+# to clients depending on their
 # conditions.
 # ==================================
 
-#Initialize the people
-victim = ["9999999999", "Shelter"]
+# Precondition: Volunteer and client will share the same language and location.
 
-volunteer = [["1111111111", "Food"],
-             ["2222222222", "Law"],
-             ["3333333333", "Shelter"],
-             ["4444444444", "Shelter"]
+# 1 client that chooses from list of volunteers
+# If there is no available volunteer, then it goes to the next
+# volunteer with the least amount of clients.
+
+#Initialize the people
+client = ["c9", "Shelter", "Law"]
+
+volunteer = [["v1", "Food"],
+             ["v2", "Law"],
+             ["v3", "Food", "Shelter"],
+             ["v4", "Shelter"],
+             ["v5", "Shelter", "Law"]
 ]
 
-volunteer_score = []
-print victim
-print volunteer
-print volunteer_score
+volunteer_score = [0]*(len(volunteer))
+#print client
+#print volunteer
+#print volunteer_score
 
 index = 0
-for x in volunteer:
-    if victim[1] == x[1]:
-        volunteer_score.insert(0,"Match found at {0}".format(x))
-        index = index + 1
+search_index = 1
+while search_index < (len(client)):
+    for i in volunteer: # check individual volunteers
+        print "client search index is: " + str(client[search_index])
+        print "i is: " + str(i)
+        for j in i: # check each individual trait
+            print "\tj is: " + str(j)
+            if client[search_index] == j:
+                print "\t\tInserting: " + str(client[search_index]) + "in " + str(index)
+                volunteer_score[index] = 1
+        print "j loop complete."
+        index += 1
+    print "i loop complete.\n"
+    search_index += 1
+print "while loop complete. search_index = "+ str(search_index)
 
-
+    
 
 
 print volunteer_score
-
-
-# Precondition: Volunteer and Victim will share the same language.
-
-# 1 VICTIM
-# Choose from list of volunteers
-# If there is no available volunteer, then it goes to the next
-# volunteer with the least amount of victims.
-# City
-# Tags
-
-
-
-#function stableMatching {
-#    Initialize all m E M and w E W to free
-#    while 3 free man m who still has a woman w to propose to {
-#       w = first woman on m's list to whom m has not yet proposed
-#       if w is free
-#         (m, w) become engaged
-#       else some pair (m', w) already exists
-#        if w prefers m to m'
-#            m' becomes free
-#           (m, w) become engaged 
-#         else
-#           (m', w) remain engaged
-#    }
-#}
