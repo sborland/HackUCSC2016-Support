3bd671646b84118c81ef92aa3788bbce2674eb75
Henry Pan <henry-pan@users.noreply.github.com>
2016-01-29 20:33:46 -0800

    Almost done

--- Matching.py
+++ Matching.py
@@ -17,38 +17,45 @@
 # volunteer with the least amount of clients.
 
 #Initialize the people
-client = ["c9", "Shelter", "Law"]
 
-volunteer = [["v1", "Food"],
-             ["v2", "Law"],
-             ["v3", "Food", "Shelter"],
-             ["v4", "Shelter"],
-             ["v5", "Shelter", "Law"]
+
+client = ["c9", "Food", "Shelter", "Law"]
+
+volunteer = [["v0", "Food"],
+             ["v1", "Law"],
+             ["v2", "Food", "Shelter"],
+             ["v3", "Shelter"],
+             ["v4", "Shelter", "Law"],
+             ["v5", "Food", "Law"],
+             ["v6", "Shelter", "Law", "Food"]
 ]
 
-volunteer_score = [0]*(len(volunteer))
-#print client
-#print volunteer
-#print volunteer_score
+def matchVolunteer():
+    volunteer_score = [0]*(len(volunteer))
+    index = 0
+    search_index = 1
+    while search_index < (len(client)):
+        for i in volunteer: # check individual volunteers
+            #print "client search index is: " + str(client[search_index])
+            #print "i is: " + str(i)
+            for j in i: # check each individual trait
+                #print "\tj is: " + str(j)
+                if client[search_index] == j:
+                    #print "\t\tInserting: " + str(client[search_index]) + " in " + str(index)
+                    volunteer_score[index] += 1
+            #print "j loop complete."
+            index += 1
+        #print "i loop complete.\n"
+        index = 0
+        search_index += 1
+    #print "while loop complete. search_index = "+ str(search_index)
 
-index = 0
-search_index = 1
-while search_index < (len(client)):
-    for i in volunteer: # check individual volunteers
-        print "client search index is: " + str(client[search_index])
-        print "i is: " + str(i)
-        for j in i: # check each individual trait
-            print "\tj is: " + str(j)
-            if client[search_index] == j:
-                print "\t\tInserting: " + str(client[search_index]) + "in " + str(index)
-                volunteer_score[index] = 1
-        print "j loop complete."
-        index += 1
-    print "i loop complete.\n"
-    search_index += 1
-print "while loop complete. search_index = "+ str(search_index)
+    highest_score = 0
+    for i in volunteer_score:
+        if i > highest_score:
+            highest_score = i
 
-    
+ 
+    result = volunteer[volunteer_score.index(highest_score)]
 
-
-print volunteer_score
+    return result[0]
