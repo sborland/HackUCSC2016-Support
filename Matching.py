d04c14e7048f5426b47f1d5e0fcfcc43fd946e53
Henry Pan <henry-pan@users.noreply.github.com>
2016-01-29 17:08:37 -0800

    progress

--- Matching.py
+++ Matching.py
@@ -1,21 +1,45 @@
+# ==================================
+# Matching.py
+# 
+# Matching.py [Victim's Attributes] [[Volunteer's Atts],...]
+# 
+# 
+# This algorithm will match volunteers
+# to victims depending on their
+# conditions.
+# ==================================
+victim = [4151234567, "Santa Cruz", "Shelter"]
+
+print victim
 
 
 
 
 
-""""
-function stableMatching {
-    Initialize all m ∈ M and w ∈ W to free
-    while ∃ free man m who still has a woman w to propose to {
-       w = first woman on m’s list to whom m has not yet proposed
-       if w is free
-         (m, w) become engaged
-       else some pair (m', w) already exists
-         if w prefers m to m'
-            m' becomes free
-           (m, w) become engaged 
-         else
-           (m', w) remain engaged
-    }
-}
-""""
+# Precondition: Volunteer and Victim will share the same language.
+
+# 1 VICTIM
+# Choose from list of volunteers
+
+# If there is no available volunteer, then it goes to the next
+# volunteer with the least amount of victims.
+
+# City
+# Tags
+
+
+
+#function stableMatching {
+#    Initialize all m E M and w E W to free
+#    while 3 free man m who still has a woman w to propose to {
+#       w = first woman on m's list to whom m has not yet proposed
+#       if w is free
+#         (m, w) become engaged
+#       else some pair (m', w) already exists
+#        if w prefers m to m'
+#            m' becomes free
+#           (m, w) become engaged 
+#         else
+#           (m', w) remain engaged
+#    }
+#}
