c343acd55d6f916cf790a5f03ffadb344e268334
Henry Pan <henry-pan@users.noreply.github.com>
2016-01-29 16:24:28 -0800

    Initial
    checkem

---
+++ Matching.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+""""
+function stableMatching {
+    Initialize all m ∈ M and w ∈ W to free
+    while ∃ free man m who still has a woman w to propose to {
+       w = first woman on m’s list to whom m has not yet proposed
+       if w is free
+         (m, w) become engaged
+       else some pair (m', w) already exists
+         if w prefers m to m'
+            m' becomes free
+           (m, w) become engaged 
+         else
+           (m', w) remain engaged
+    }
+}
+""""
