449c00f9df524445bf13007621bdfb199291c3de
sborland <sborland@ucsc.edu>
2016-01-29 22:19:00 -0800

    Keep on chuging on

--- ServiceWeb/volunteerform.py
+++ ServiceWeb/volunteerform.py
@@ -1,19 +1,21 @@
-import web
 from flask import request, redirect
+import web
+from flask import Flask
+app = Flask(__name__)
 
-urls = (
-  '/', 'signup'
-)
+app = web.application(globals())
 
-app = web.application(urls, globals())
-
-render = web.template.render('templates/')
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 @app.route('/signup', methods = ['POST'])
 def signup():
+    print("HERE!")
     email = request.form['email']
     print("The email address is '" + email + "'")
     return redirect('/')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='127.0.0.1', port=8020)
+    
