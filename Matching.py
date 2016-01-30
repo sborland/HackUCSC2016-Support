e3f5dc21cfcf0cb541dde06e25f35f5520dcd68b
Henry Pan <henry-pan@users.noreply.github.com>
2016-01-29 18:08:49 -0800

    Before food

--- Matching.py
+++ Matching.py
@@ -3,27 +3,45 @@
 # 
 # Matching.py [Victim's Attributes] [[Volunteer's Atts],...]
 # 
-# 
+# Lists must have at least three attributes 
+#
 # This algorithm will match volunteers
 # to victims depending on their
 # conditions.
 # ==================================
-victim = [4151234567, "Santa Cruz", "Shelter"]
 
+#Initialize the people
+victim = ["9999999999", "Shelter"]
+
+volunteer = [["1111111111", "Food"],
+             ["2222222222", "Law"],
+             ["3333333333", "Shelter"],
+             ["4444444444", "Shelter"]
+]
+
+volunteer_score = []
 print victim
+print volunteer
+print volunteer_score
+
+index = 0
+for x in volunteer:
+    if victim[1] == x[1]:
+        volunteer_score.insert(0,"Match found at {0}".format(x))
+        index = index + 1
 
 
 
+
+print volunteer_score
 
 
 # Precondition: Volunteer and Victim will share the same language.
 
 # 1 VICTIM
 # Choose from list of volunteers
-
 # If there is no available volunteer, then it goes to the next
 # volunteer with the least amount of victims.
-
 # City
 # Tags
 
