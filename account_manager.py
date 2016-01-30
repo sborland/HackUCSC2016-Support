1f2a3e82b3b60892afe645354d44a2beb37a4457
Karl Cassel <thee1gameboy@yahoo.com>
2016-01-29 20:19:45 -0800

    cases probably working
    half coded script, don't wanna mess anything up

--- account_manager.py
+++ account_manager.py
@@ -3,8 +3,56 @@ import sys
 from array import array
 from sys import argv
 
+# Structure for the volunteer object.
+class volunteer(object):
+    def __init__(self, title):
+        self.title = title
+    phone = None
+    email = None
+    client = None
+    language = None
+    tagArray = [None]*50
+
+
+# Each of the cases for lines.
+def fileCases(arg, file, Obj):
+    if arg == "phone":
+        Obj.phone = file.readline()
+    elif arg == "email":
+        Obj.email = file.readline()
+    elif arg == "client":
+        Obj.client = file.readline()
+    elif arg == "language":
+        Obj.language = file.readline()
+    elif arg == "tagArray":
+        i = -1
+        while (arg != "\n"):
+            Obj.tagArray[++i] = file.readline()
+    else:
+        return "break"
+
+
+# Command line arguments
 script, filename = argv
 
-try:
+# Initialization of volunteer object.
+volunteerObj = volunteer(["title"])
 
-    con = lite.connect('serviceDatabase.db')
+try:
+    # Reading in the database file.
+    con = lite.connect('service_database.db')
+    cur = con.cursor()
+    file = open(filename)
+
+    # Creating the table with the elements
+    cur.execute('''CREATE TABLE  volunteers(PhoneNum TEXT, Email TEXT, Client TEXT, Language TEXT, Tags TEXT)''')
+
+    # Read the file and deal with each of the cases
+    # If we hit the end of the file, then break.
+
+    while True:
+        nextLine = file.readline()
+
+        if(fileCases(nextLine, file, volunteerObj) == "break"):
+            break
+
